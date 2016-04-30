959aeefceb5429438b4add30e9ea37e23ff91ee6
Stew Higham <stew.higham@gmail.com>
2016-04-30 09:45:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
-Pass
+from setuptools import setup
+
+setup(name='probo', version='0.1', packages=['probo'], author='Stew Higham and Tyler Brough',
+        author_email = "stooge4412@gmail.com", description="The Option Pricing Module",
+        keywords = "option pricing object-oriented design patterns Monte Carlo Binomial Black-Scholes",
+        url="https://github.com/lshigham/probo")
