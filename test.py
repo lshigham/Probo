b1dd29a8faf41f151ce2b0564ae4825c70d85fbe
Stew Higham <stew.higham@gmail.com>
2016-04-30 10:58:24 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-#Create Test for module.
+#Edits needed
+
+import marketData 
+import payoff 
+import pricingEngine
+import Facade
+
+def main():
+    strike = 40.0
+    expiry = .25 
+    
+    spot = 41.0
+    rate = 0.08
+    volatility = 0.30
+    dividend = 0.0
+    steps = 500 
+
+    call = Vanilla_Payoff(expiry, strike, call_payoff)
+    data = MarketData(rate, spot, volatility, dividend)
+    binom_engine = BinomialPricingEngine(steps, EuropeanBinomialPricer)
+    
+    the_option = OptionFacade(call, binom_engine, data)
+    price = the_option.price()
+    print("The Call Price is {0:.3f}".format(price))
+    
+if __name__ == "__main__":
+    main()
