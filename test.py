c5587bf10599a417d3a645ea2d0a1e0726edea5a
Stew Higham <stew.higham@gmail.com>
2016-05-04 05:00:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,36 @@
-#Edits needed
-from probo.marketData import MarketData
+from probo.marketdata import MarketData
 from probo.payoff import VanillaPayoff, call_payoff
-from probo.pricingEngine import BinomialPricingEngine, EuropeanBinomialPricer
-from probo import Facade
+from probo.engine import BinomialPricingEngine, EuropeanBinomialPricer, BlackScholesPricingEngine, BlackScholesPricer
+from probo.facade import OptionFacade
 
 
 def main():
+    """Set up the option!"""
     strike = 40.0
     expiry = .25 
     
+    """Set up the Market Data!"""
     spot = 41.0
     rate = 0.08
     volatility = 0.30
     dividend = 0.0
+    
+    """Set up the European Binomial Pricing Engine!"""
     steps = 500 
-
-    call = Vanilla_Payoff(expiry, strike, call_payoff)
+    call = VanillaPayoff(expiry, strike, call_payoff)
     data = MarketData(rate, spot, volatility, dividend)
     binom_engine = BinomialPricingEngine(steps, EuropeanBinomialPricer)
     
+    """Calculate the Price"""
     the_option = OptionFacade(call, binom_engine, data)
     price = the_option.price()
     print("The Call Price is {0:.3f}".format(price))
+    
+    """Set up Black Scholes Price!"""
+    the_call = VanillaPayoff("call", BlackScholesPricer)
+    BS_option = OptionFacade(the_call, BS_engine, data)
+    BS_price = BS_option.price()
+    print("The call price via BLack Scholes is:  {0:.3f}".format(BS_price))
     
 if __name__ == "__main__":
     main()
