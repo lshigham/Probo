a8dfed16206f1c8b4d0ca52f80754af76c1e68b4
Stew Higham <Stew Higham>
2016-05-06 08:50:36 -0600

    Problem 3 add

---
+++ projprob3.py
@@ -0,0 +1,27 @@
+from probo.marketdata import MarketData
+from probo.payoff import *
+from probo.engine import *
+from probo.facade import OptionFacade
+
+def main():
+    """Set up the option!"""
+    strike = 40.0
+    expiry = .25 
+    
+    """Set up the Market Data!"""
+    spot = 41.0
+    rate = 0.08
+    volatility = 0.30
+    dividend = 0.0
+    
+    """Set up the Pricing Engine!"""
+    time_steps = 50
+    replications =100
+    
+    call = ExoticPayoff(expiry, strike, arithmetic_asian_call_payoff)
+    data = MarketData(rate, spot, volatility, dividend)
+    
+    Arithmetic_A_engine = MonteCarloPricingEngine(time_steps, replications, Asian_Option_Pricer)
+    AA_option = OptionFacade(call, Arithmetic_A_engine, data)
+    AA_price = AA_option.price()
+    print("The call price for an Arithmetic Asian Call Option using a Geometric Asian control Variate is:  {0:.3f}".format(AA_price))
